70eab0d1ae36f72e60e7ad5f0f30b6960436e6ce
kema-ray <rachelkemuma99@gmail.com>
2023-08-31 11:40:57 +0300

    create a special offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,13 +8,13 @@ const Button = ({
 }) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      className={`flex justify-center items-center gap-2 px-7
+      py-4 border font-montserrat text-lg leading-none
       ${
         backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor}`
           : "bg-coral-red text-white border-coral-red"
-      } rounded-full ${fullWidth && "w-full"}`}
-    >
+      } rounded-full ${fullWidth && "w-full"}`}>
       {label}
 
       {iconURL && (
